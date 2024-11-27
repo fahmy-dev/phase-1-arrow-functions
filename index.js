b4ece1a50612d840fccc160855b831754078e2a2
fahmy-dev <faahmy@proton.me>
2024-11-27 14:48:54 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = divide => {
+    return 2000 / 100;
+}
+
+const square = square => {
+    let x = 2;
+    return square * 2;
+}
+
+const add = add => {
+   const num1 = 3
+   const num2 = 4
+   return num1 + num2; 
+}
